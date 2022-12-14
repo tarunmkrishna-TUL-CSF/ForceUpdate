ab775bb9fc2ee36d2035f7ab4128cabe444d04d7
tarunmkrishna2712 <tkrishna@tataunistore.com>
2022-12-14 15:17:18 +0530

    add framework

---
+++ ForceUpdate/ForceUpdate/TCForceUpdateAlert.swift
@@ -0,0 +1,43 @@
+//
+//  TCForceUpdateAlert.swift
+//  ForceUpdate
+//
+//  Created by Tarun M Krishna on 14/12/22.
+//
+
+import UIKit
+
+enum ForceUpdateType {
+    case forceUpdate
+    case softNudge
+    case na
+}
+
+struct TCForceUpdateAlert {
+    var updateType: ForceUpdateType = .na
+    
+    mutating func determineForceUpdate(versions: [String], currentVersion: String) -> ForceUpdateType {
+        if currentVersion == "a" {
+            updateType = .forceUpdate
+        } else if currentVersion == "b" {
+            updateType = .softNudge
+        } else {
+            updateType = .na
+        }
+        return updateType
+    }
+    
+    private func showForceUpdateAlert(appName: String, updateURL: String) -> UIAlertController {
+        let alert = UIAlertController(title: "Update Alert!", message: "\(appName) has new release 🔔🔔, please update to explore latest changes", preferredStyle: .alert)
+        if updateType == .softNudge {
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        }
+        alert.addAction(UIAlertAction(title: "Update", style: .default, handler: { _ in
+            print("Alert updated")
+//            if let appURL = URL(string: updateURL) {
+//                UIApplication.shared.open(appURL)
+//            }
+        }))
+        return alert
+    }
+}
